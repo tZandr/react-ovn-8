09869196ab831375136ce48cdf2a63f4f3d43a31
tZandr <alex_tjernstrom@hotmail.com>
2026-04-16 23:58:10 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div id="root">
+        <div className="header">
+          <h2>React - Övning 4</h2>
+          <p>x</p>
+        </div>
+        <div className="assignment">
+        </div>
+        <div>
+          <small>Alexander Tjernström, IT-Högskolan JSU25</small>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
